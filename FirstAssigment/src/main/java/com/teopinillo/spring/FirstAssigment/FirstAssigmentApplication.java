c0c0e22a399fb0898f64d6a10a08dd454912787c
teopinillo <teopinillo@gmail.com>
2020-02-07 01:50:58 -0500

    First Assignment in the Udemy Spring Course.

---
+++ FirstAssigment/src/main/java/com/teopinillo/spring/FirstAssigment/FirstAssigmentApplication.java
@@ -0,0 +1,13 @@
+package com.teopinillo.spring.FirstAssigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAssigmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAssigmentApplication.class, args);
+	}
+
+}
---
+++ FirstAssigment/src/test/java/com/teopinillo/spring/FirstAssigment/FirstAssigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teopinillo.spring.FirstAssigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAssigmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
